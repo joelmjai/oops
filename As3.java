a37e610262cae431d4eeb0b3ae4b57927a035d44
joelmjai <73684796+joelmjai@users.noreply.github.com>
2021-02-10 10:49:58 +0530

    Update As3.java

--- As3.java
+++ As3.java
@@ -1,7 +1,10 @@
 import java.io.*;
+import java.util.Scanner;
 public class P3{
   public static void main(String args[]){
-     int n1=5; 
+     int n1;
+     Scanner sc=new Scanner(System.in);
+     n1=sc.nextInt();
      if(n1%2==0)
      	System.out.println("The Number is Even");
      else
