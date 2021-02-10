ac4df0819c76bbd3399d092c5d64c807ffedff2a
joelmjai <73684796+joelmjai@users.noreply.github.com>
2021-02-10 10:09:52 +0530

    Update As2.java

--- As2.java
+++ As2.java
@@ -1,7 +1,11 @@
-import java.io.*;
+import java.util.Scanner;
 public class P2{
   public static void main(String args[]){
-     int n1=5,n2=6,sum;
+     int n1,n2,sum;
+     Scanner sc=new Scanner(System.in);
+     n1=sc.nextInt();
+     n2=sc.nextInt();
+     sc.close();
      sum=n1+n2;
      System.out.println("Sum ="+sum);
 }
